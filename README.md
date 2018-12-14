c7b64a634e04d6d07f94ced0a4ac045fd929c44a
DoryDuan <tyhduanjinli@163.com>
2018-12-14 17:34:59 +0800

    登录注册

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 ## mui
 [mui初级入门教程（二）— html5+ webview 实现底部](http://ask.dcloud.net.cn/article/650)
 
+通过mui.open方法打开之前的一个页面，没有动画。如：登录页面->注册页面->通过mui.open方法到登录页面
+
+## 移动端300毫秒延迟
+1.引入fastclick插件
+2.用mui封装好的tab事件代替click事件
+
+## mui 
+ajax全局调用封装
+参考[jquery Ajax 全局调用封装](https://blog.csdn.net/xllily_11/article/details/51567186)
+[jquery Ajax 全局调用封装](https://bbs.csdn.net/topics/330015234)
+
+微信登录
+[微信登录资料1](http://www.hcoder.net/course/info_240.html)
+[微信登录资料2](http://ask.dcloud.net.cn/article/12624)
+[微信登录资料3](http://ask.dcloud.net.cn/article/192)
+
+## vue
+ [vue+checkbox/radio的用法](https://vuejs.org/v2/guide/forms.html#Checkbox)
+ 
+## form表单
+点击form标签包裹的button，需要加上属性action="javascript:;"不然页面会刷新
+http://www.hcoder.net/course/info_240.html
---
+++ js/mui-ajax.js
@@ -0,0 +1,32 @@
+var BASE_URL='http://115.236.33.6:3129';
+function muiRequest(options) {
+	var TIMEOUT = 10000;
+//	var token = plus.storage.getItem('userInfo').token || '';
+	var token = ''
+	options.successFn = options.successFn || function() {};
+//	options.errorFn = options.errorFn || function() {};
+    mui.ajax(BASE_URL+options.url,{
+    	data: options.data || null,
+    	dataType: options.dataType || "json",
+        type: options.type || "get",
+        timeout: options.timeout || TIMEOUT, //超时时间设置为10秒；
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+			'X-AUTH-TOKEN': token
+		},
+        success: function(data){
+            options.successFn(data);
+        },
+        error: function(xhr,type,errorThrown){
+        	console.log(xhr)
+            if(xhr.status == 401) {
+				mui.openWindow({
+					id: 'login',
+					url: 'login.html'
+				});
+			} else {
+//				mui.toast(xhr.message)
+			}
+        }
+    });
+}
